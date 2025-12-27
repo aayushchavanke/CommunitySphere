f2cf596f0197e6a2c76826bd68c7d61d0356ba4f
Aayush Chavanke <156324718+aayushchavanke@users.noreply.github.com>
2025-12-27 10:24:43 +0530

    Update database host in config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
-define("DB_HOST", "terraform-20251226043828104900000009.c1uy86oaiqum.ap-south-1.rds.amazonaws.com");
+define("DB_HOST", ""terraform-20251227043954517800000007.c1uy86oaiqum.ap-south-1.rds.amazonaws.com");
 define("DB_USER", "admin");          
 define("DB_PASS", "password1234"); 
 define("DB_NAME", "communitydb");    
 ?>
+
